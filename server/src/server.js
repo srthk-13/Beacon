0fc8c56bc1b97f94263d6eaa69f07d0476deb282
rudra-2027 <144617358+rudra-2027@users.noreply.github.com>
2026-03-25 20:21:04 +0530

    refactor: move MongoDB connection to separate config file

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,12 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
-dotenv.config({ path: "../.env" });
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Database connection failed:", err);
-  });
+connectDB();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
